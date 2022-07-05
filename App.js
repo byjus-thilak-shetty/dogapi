2d7ff418a28194501ce121cd11c5b37b0516d3bf
thilak-shetty <108680420+thilak-shetty@users.noreply.github.com>
2022-07-05 15:40:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,26 +7,37 @@ import './App.css';
 
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            loading: true,
-            dogs :null,
-            dogBreed:null
-        }
-    }
+    state={
+        loading:false,
+        dogs:[],
+        dogBreed:null
+    };
 
-    async componentDidMount(){
-        const url="https://api.thedogapi.com/v1/images/search?api_key=af6f2d6f-7cd4-421e-8fd9-83188dd6e69b";
-        const response= await fetch(url);
-        const data= await response.json();
-        console.log(data);
-        this.setState({dogs: data[0].url, loading:false, dogBreed:data[0].breeds[0]});
+    // async componentDidMount(){
+    //     const url="https://api.thedogapi.com/v1/breeds?api_key=af6f2d6f-7cd4-421e-8fd9-83188dd6e69b";
+    //     const response= await fetch(url);
+    //     const data= await response.json();
+    //     console.log(data);
+    //     this.setState({dogs: data.name, loading:false});
+    // }
+    componentDidMount() {
+        fetch(
+"https://api.thedogapi.com/v1/breeds?api_key=af6f2d6f-7cd4-421e-8fd9-83188dd6e69b")
+            .then((res) => res.json())
+            .then((json) => {
+                json.forEach(element=>{
+                    const {name}=element
+                    this.state.dogs.push({name: name})
+                })
+            })
     }
 
     render() { 
+        const { loading, dogs} = this.state;
         return (
-        <div>
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  
+            <h2>{dogs.name}</h2>
             <DogList dogs={this.state.dogs}/>
         </div>
         );
